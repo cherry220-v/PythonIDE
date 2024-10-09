8e4dd5b3aee230f704ac073fc2e93b6620c79556
cherry220-v <crispycrabber@gmail.com>
2024-10-10 00:10:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+pyFile = open("pythonpath", "a+")
+
+def pythonPath():
+    return pyFile.read().split("\n")[0]
+
+def initAPI(api):
+    global vtapi, platform, os, CustomDialog
+    vtapi = api
+    m = vtapi.FSys
+    os = m.osModule()
+
+    import sys
+
+    from PyQt6.QtCore import QProcess
+
+    from dialogClass import CustomDialog
+
+def runFile():
+    tab = vtapi.Tab.currentTabIndex()
+    process = QProcess()
+
+    script_path = vtapi.Tab.getTabFile(tab)
+    sys.path.insert(0, pythonPath())
+
+    if os.name == 'nt':  # Если Windows
+        cmd_command = f"start cmd.exe /k python {script_path} & timeout /t -1"
+        process.start("cmd.exe", ["/c", cmd_command])
+    else:  # Если Linux или другие ОС
+        bash_command = f"bash -c \"python3 {script_path}; read -p 'Press Enter to continue...'\""
+        process.start("x-terminal-emulator", ["-e", bash_command])
+
+    process.waitForFinished()
+
+def showPythonPath():
+    d = CustomDialog(vtapi)
+    d.exec()
